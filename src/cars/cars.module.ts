dc0efab8eb633783600441610af8432a207edf82
MarcoBaygorria <baygorria91@gmail.com>
2025-12-23 22:42:28 -1100

    services

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
 import { CarsController } from './cars.controller';
+import { CarsService } from './cars.service';
 
 @Module({
     controllers: [CarsController],
+    providers: [CarsService],
 })
-export class CarsModule { }
+export class CarsModule {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        },
+        {
+            id: 2,
+            brand: 'Honda',
+            model: 'Civic'
+        },
+        {
+            id: 3,
+            brand: 'Jeep',
+            model: 'Cherokee'
+        },
+    ]
+}
